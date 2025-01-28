91327dca894f2c07277188d12e8371ce1a8921fc
AnnWann <uanabgasparim@gmail.com>
2025-01-27 22:55:00 -0300

    feat/crud/criar AND feat/crud/listar

--- src/gerenciadorDeDataShow/crud.clj
+++ src/gerenciadorDeDataShow/crud.clj
@@ -3,6 +3,9 @@
  
 ; Uaná
 ; criar (id) -> cria um novo datashow com horario "indefinido"
+; Vou assumir que tem um agente para a lista chamado ListaDeDatashows
+(defn criar [id]
+  (send-off ListaDeDatashows (conj ListaDeDatashows {:id id :horario "indefinido"})))
 
 ; Alvaro
 ; alocar (id, horario) -> aloca datashow(id) a um horario
@@ -18,6 +21,8 @@
 
 ; Uaná
 ; listar () -> lista todos os datashows
-
+(defn listar []
+  (await ListaDeDatashows)
+  (@ListaDeDatashows))
 
 
