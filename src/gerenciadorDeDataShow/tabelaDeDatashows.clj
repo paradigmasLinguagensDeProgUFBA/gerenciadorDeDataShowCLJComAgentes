585160b2fc7e05f231d70ae6ef752eaa0a7343b3
otavio oliveira <otavio14novais@gmail.com>
2025-01-28 17:22:11 -0300

    Upadate tabelaDeDatashows

--- src/gerenciadorDeDataShow/tabelaDeDatashows.clj
+++ src/gerenciadorDeDataShow/tabelaDeDatashows.clj
@@ -6,3 +6,29 @@
 ; Otavio
 ; listaDeDatashows -> definição de lista de datashows (datashows)
 
+
+
+;; Função para criar um datashow
+(defn criar-datashow [id horario status]
+  {:id id
+   :horario horario
+   :status status})
+
+;; Agente para gerenciar o vetor de datashows
+(def ListaDeDatashows (agent []))
+
+;; Função para adicionar um datashow ao vetor
+(defn adicionar-datashow [vetor datashow]
+  (conj vetor datashow))
+
+;; Função para atualizar o status de um datashow
+(defn atualizar-status [vetor id novo-status]
+  (mapv (fn [datashow]
+          (if (= (:id datashow) id)
+            (assoc datashow :status novo-status)
+            datashow))
+        vetor))
+
+;; Função para remover um datashow pelo ID
+(defn remover-datashow [vetor id]
+  (filterv #(not= (:id %) id) vetor))
